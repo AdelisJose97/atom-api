1a2cf706967ca9b52e0e80db7de17ba48c412db8
AdelisJose97 <adelisjose1997@gmail.com>
2024-05-31 20:07:02 -0400

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express'
 var cors = require('cors')
-import taskRoutes from './routes/tasks';
-import usersRoutes from './routes/users';
-import 'dotenv/config';
+import taskRoutes from './routes/tasks'
+import usersRoutes from './routes/users'
+import 'dotenv/config'
 
-
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
 app.use(cors())
-app.use(express.json());
-app.use('/tasks', taskRoutes);
-app.use('/users', usersRoutes);
+app.use(express.json())
+app.use('/tasks', taskRoutes)
+app.use('/users', usersRoutes)
+app.get('/', (req, res) => {
+  return res.json({ message: 'Api running' })
+})
 
 app.listen(port, () => {
-  console.log(`Server running at port => ${port}`);
-});
+  console.log(`Server running at port => ${port}`)
+})
